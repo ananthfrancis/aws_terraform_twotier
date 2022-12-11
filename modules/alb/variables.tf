3cdbd47eb1795a2ac5442cee8b93bd3607e154b6
ananth <dol_engg2@devopslabs.tech>
2022-12-11 22:23:26 +0800

    updated variables

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,76 +1,71 @@
 
 variable "alb_name" {
-  description = "A tenancy option for instances launched into the VPC"
+  description = "ALB Name"
   type        = string
   default     = "default"
 }
 
 variable "security_group_ids" {
-  description = "Suffix to append to public subnets name"
+  description = "Security Group IDs"
   type        = list(string)
 }
 
 variable "subnets" {
-  description = "Suffix to append to private subnets name"
+  description = "List of Subnet IDs where the ALB will be provisioned"
   type        = list(string)
 }
 
 
 variable "cross_zone_load_balancing" {
-  description = "A tenancy option for instances launched into the VPC"
+  description = "Cross zone load balancing required or not"
   type        = bool
 }
 
 variable "lb_port" {
-  description = "Suffix to append to public subnets name"
+  description = "LB port number"
   type        = string
   default     = "public"
 }
 
 variable "lb_protocol" {
-  description = "Suffix to append to private subnets name"
+  description = "LB Protocol. Only HTTP or HTTPS allowed"
   type        = string
   default     = "private"
 }
 
 variable "instance_port" {
-  description = "Suffix to append to public subnets name"
+  description = "Instance Port number"
   type        = string
   default     = "public"
 }
 
 variable "instance_protocol" {
-  description = "Suffix to append to private subnets name"
+  description = "Instance Protocol. Only HTTP or HTTPS allowed"
   type        = string
   default     = "private"
 }
 
 variable "vpc_id" {
-  description = "Suffix to append to public subnets name"
+  description = "VPC ID"
   type        = string
 }
 
 variable "instance_id" {
-  description = "Suffix to append to public subnets name"
+  description = "Instance ID value"
   type        = string
 }
 
 variable "acm_arn" {
-  description = "Suffix to append to private subnets name"
+  description = "ACM arn value to be used in HTTPS listener of ALB"
   type        = string
 }
 
-# variable "zone_id" {
-#   description = "Suffix to append to private subnets name"
-#   type        = string
-# }
-
 variable "root_domain_name" {
-  description = "Suffix to append to private subnets name"
+  description = "root domain name to be used for configuring route53 record"
   type        = string
 }
 
 variable "domain_name" {
-  description = "Suffix to append to private subnets name"
+  description = "subdomain record to be created in the public hosted zone"
   type        = string
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,13 +1,17 @@
 variable "ami" {
+  description = "AMI ID to be used for creating EC2 instance"
   type    = string
 }
 variable "instance_type" {
+  description = "Instance type for EC2 instance"
   type    = string
 }
 variable "az" {
+  description = "Availability Zone"
   type    = string
 }
 variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
   type    = string
 }
 
@@ -17,9 +21,11 @@ variable "tags" {
 }
 
 variable "security_group_ids" {
+    description = "Security Group ID for the EC2 instance"
     type = any
 }
 
 variable "ec2_name" {
+  description = "Name of the ec2 instance"
   type    = string
 }
